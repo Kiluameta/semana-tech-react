f2168cafec97b09ce539eff46553463cd0ac23cc
Igor Gabriel <rogiborgessouza@outlook.com>
2024-08-08 16:18:11 -0300

    config: react-query and sonner toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { CreateRoom } from './pages/create-room'
 import { Room } from './pages/room'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
+import { Toaster } from 'sonner'
 
 const router = createBrowserRouter([
   {
@@ -14,6 +17,11 @@ const router = createBrowserRouter([
 ])
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <Toaster invert richColors />
+    </QueryClientProvider>
+  )
 }
 
